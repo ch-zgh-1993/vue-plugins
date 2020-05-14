675bd6c746b49ac439e0e2ff5ee6daa38a07e929
zhangguohua <zhangguohua@shuidihuzhu.com>
2020-05-14 20:51:11 +0800

    feat: 开发兼容多个模块分别控制加载时机

--- src/plugins/scroll-load.vue
+++ src/plugins/scroll-load.vue
@@ -2,13 +2,21 @@
 * @Author: Zhang Guohua
 * @Date:   2020-04-29 10:17:46
 * @Last Modified by:   zgh
-* @Last Modified time: 2020-05-08 14:50:12
+* @Last Modified time: 2020-05-14 20:45:07
 * @Description: create by zgh
 * @GitHub: Savour Humor
 -->
 <template>
-	<div ref="wrapper">
-		<slot v-if="judgeShow"></slot>
+	<div>
+		<div 
+			v-for="(item, index) in slotArr"
+			:key="index"
+			:ref="item.name">
+			<slot 
+				v-slot:[item.name]
+				v-if="item.showBlock !== null ? item.showBlock : item.show">
+			</slot>
+		</div>
 	</div>
 </template>
 
@@ -23,42 +31,84 @@ export default {
 		},
 		bottom: {
 			type: Number,
-			default: false
+			default: null
+		},
+		slotProps: {
+			type: Array,
+			default () {
+				return []
+			}
 		}
 	},
 	data () {
 		return {
-			show: false,
 			ticking: false,
-			height: 100
+			viewPortHeight: null,
+			slotArr: []
 		}
 	},
-	computed: {
-		showHeight () {
-			const viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-			const bottom = this.bottom !== false ? this.bottom : this.height
-			return viewPortHeight + bottom
-		},
-		judgeShow () {
-			const show = this.showBlock !== null ? this.showBlock : this.show
-			show && this.$emit('load')
-			return show
-		}
+	created () {
+		this.slotArr = this.slotProps.length > 0 ? this.formatSlotProps() : [{
+			name: 'default',
+			bottom: this.bottom || 100,
+			showBlock: this.showBlock === null ? null : this.showBlock,
+			show: false
+		}]
 	},
 	mounted () {
-		this.showBlock === null && document.addEventListener('scroll', this.watchScroll, false)
+		this.addListener()
+	},
+	watch: {
+		showBlock (val) {
+			this.slotProps.length == 0 && (this.slotArr[0].showBlock = !!val)
+		}
 	},
 	methods: {
-		watchScroll (event){
+		formatSlotProps () {
+			const slotProps = this.slotProps
+			slotProps.every(val => val.name !== 'default') && slotProps.push({
+				name: 'default',
+				bottom: 100,
+				showBlock: null,
+				show: false
+			})
+			return slotProps.map(val => {
+				if (typeof val !== 'string' && Object.prototype.toString.call(val) !== '[object Object]') throw new Error('slotProps must be Array<string | object>')
+				const base = {
+					showBlock: null,
+					show: false,
+					bottom: 100
+				}
+				return typeof val === 'string' ? {
+					...base,
+					name: val
+				} : {
+					...base,
+					...val
+				}
+			})
+		},
+		judgeShow (val) {
+			return val.showBlock !== null ? val.showBlock : val.show
+		},
+		addListener () {
+			document.addEventListener('scroll', this.watchScroll, false)
+		},
+		watchScroll (){
 			const vm = this
-			
-			if (!vm.ticking && !vm.judgeShow) {
+			if (!vm.ticking && !vm.slotArr.every(val => vm.judgeShow(val))) {
 			    window.requestAnimationFrame(function() {
-			    	const offsetTop = vm.$refs.wrapper.offsetTop
-			    	const scrollTop = document.documentElement.scrollTop
-			    	if (offsetTop - scrollTop < vm.showHeight) {
-			    		vm.show = true
-			    	}
+			    	vm.slotArr.forEach(val => {
+			    		if(vm.judgeShow(val)) return
+			    		const offsetTop = val.name === 'default' ? (vm.$refs[val.name] && vm.$refs[val.name][0].offsetTop) : vm.$refs[val.name].offsetTop
+				    	const scrollTop = document.documentElement.scrollTop
+				    	!vm.viewPortHeight && (vm.viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight)
+
+				    	if (offsetTop - scrollTop < vm.viewPortHeight + val.bottom) {
+				    		val.show = true
+				    		vm.$emit('load', val.name)
+				    	}
+			    	})
 			      	vm.ticking = false
 			    })
 			}
@@ -66,7 +116,7 @@ export default {
 		}
 	},
 	destoryed () {
-		this.showBlock === null && document.removeEventListener('scroll', this.watchScroll, false)
+		document.removeEventListener('scroll', this.watchScroll, false)
 	}
 }
 
